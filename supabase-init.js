b45c0413caf78541c492bb0e6f61bec0004af2d9
mijnap1 <mijnap1@users.noreply.github.com>
2025-10-13 00:36:47 -0400

    initial commit

---
+++ supabase-init.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = "https://dqstskgvdiwdkonbapke.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxc3Rza2d2ZGl3ZGtvbmJhcGtlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAzMDI0NzYsImV4cCI6MjA3NTg3ODQ3Nn0.2iFEYtVQZjQOY8_sF4x0SvWIKk8L-jg4yzpXzLLFe60";
